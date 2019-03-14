ddb782e0938e1057a52d624f9198234851f64498
Insane-Hk <jonathan.triclin@gmail.com>
2019-03-14 14:19:22 +0100

    Organisation - Jonathan

---
+++ include/footer.inc.php
@@ -0,0 +1,20 @@
+<div class="gm-footer navbar navbar-fixed-bottom text-center">
+    <h3 class="gm-title">GAME<strong>MAG</strong></h3>
+    <br>
+    <div class="row">
+        <i class="gm-social fab fa-twitter-square"></i>
+        <i class="gm-social fab fa-facebook-square"></i>
+        <i class="gm-social fab fa-instagram"></i>
+        <i class="gm-social fab fa-youtube"></i>
+        <i class="gm-social fab fa-twitch"></i>
+    </div>
+    <br>
+    <div class="row">
+        <a class="gm-link" href="#">Articles</a>
+        <a class="gm-link" href="#">Classement</a>
+        <a class="gm-link" href="#">Vidéos</a>
+        <a class="gm-link" href="#">Contact</a>
+    </div>
+    <br>
+    <p class="gm-rights text-muted">@2019 GameMAG. Tout droit réservés.</p>
+</div>
---
+++ index.php
@@ -0,0 +1,19 @@
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>GameMAG</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" type="text/css" media="screen" href="css/include/footer.css">
+
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+
+    
+
+        <?php include('components/footer.inc.php'); ?>
+    </body>
+</html>
