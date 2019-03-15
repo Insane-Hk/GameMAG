d191bea15b3de6212d5a2f6d73b9aa45ad3897d2
Insane-Hk <jonathan.triclin@gmail.com>
2019-03-15 13:44:01 +0100

    envoi - Jonathan

--- include/header.inc.php
+++ include/header.inc.php
@@ -4,16 +4,32 @@
     </div>
     <div class="container text-center" style="font-family:'Fjalla one', sans-serif;">
         <nav class="Navbar-fluid">
-            <a class="navbar-brand lien active" href="#">
-                <i class="fas fa-newspaper" width="30" height="30" class="d-inline-block align-top" alt=""></i> &nbsp;
-                Article</a>
+            <?php if($_SERVER['SCRIPT_NAME']=="/gameMAG/articles.php") { ?>
+            
+                <a class="navbar-brand lien active" href="#">
+            <?php } else { ?>
+                <a class="navbar-brand lien" href="#">
+            <?php } ?>
 
-            <a class="navbar-brand lien" href="#">
+                <i class="fas fa-newspaper" width="30" height="30" class="d-inline-block align-top" alt=""></i> &nbsp;
+                Article
+            </a>
+        
+            <?php if($_SERVER['SCRIPT_NAME']=="/gameMAG/leaderboard.php") { ?>
+                <a class="navbar-brand lien active" href="#">
+            <?php } else { ?>
+                <a class="navbar-brand lien" href="#">
+            <?php } ?>
+
                 <i class="fas fa-award" width="30" height="30" class="d-inline-block align-top" alt=""></i> &nbsp;
                 Classement
             </a>
 
-            <a class="navbar-brand lien" href="#">
+            <?php if($_SERVER['SCRIPT_NAME']=="/gameMAG/videos.php") { ?>
+                <a class="navbar-brand lien active" href="#">
+            <?php } else { ?>
+                <a class="navbar-brand lien" href="#">
+            <?php } ?>
                 <i class="fas fa-video" width="30" height="30" class="d-inline-block align-top" alt=""></i>
                 &nbsp; Vidéos
             </a>
